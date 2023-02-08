2acf144c79b4372ae1e56a72deb4123f6e4df3d0
Juergen Albert <j.albert@data-in-motion.biz>
2023-02-08 14:07:20 +0100

    some smaller fixes
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- de.dim.smart.city.csv/src/de/dim/smart/city/csv/api/CSVImportService.java
+++ de.dim.smart.city.csv/src/de/dim/smart/city/csv/api/CSVImportService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012 - 2013 Data In Motion and others.
+ * Copyright (c) 2012 - 2023 Data In Motion and others.
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made
