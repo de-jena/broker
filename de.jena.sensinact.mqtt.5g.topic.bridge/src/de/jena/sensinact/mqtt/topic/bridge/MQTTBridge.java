f86114ab542b452127d4c3170638b174bb16290e
Juergen Albert <j.albert@data-in-motion.biz>
2024-06-17 17:15:22 +0200

    organized import Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- de.jena.sensinact.mqtt.5g.topic.bridge/src/de/jena/sensinact/mqtt/topic/bridge/MQTTBridge.java
+++ de.jena.sensinact.mqtt.5g.topic.bridge/src/de/jena/sensinact/mqtt/topic/bridge/MQTTBridge.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
